a30bf0f8f760b076957a4af0ca96f78dad440d73
tyarchy <ty.d.gregory@gmail.com>
2022-03-11 12:07:49 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,41 @@
+// GIVEN a command-line application that accepts user input
+// WHEN I start the application
+// THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+// WHEN I choose to view all departments
+// THEN I am presented with a formatted table showing department names and department ids
+// WHEN I choose to view all roles
+// THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
+// WHEN I choose to view all employees
+// THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+// WHEN I choose to add a department
+// THEN I am prompted to enter the name of the department and that department is added to the database
+// WHEN I choose to add a role
+// THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
+// WHEN I choose to add an employee
+// THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+// WHEN I choose to update an employee role
+// THEN I am prompted to select an employee to update and their new role and this information is updated in the database
+
+const mysql = require("mysql2");
+const inquirer = require('inquirer');
+const consoleTable = require('console.table')
+
+const db = mysql.createConnection({
+  host: "localhost",
+  port: 3001,
+  user: "root",
+  password: "^n5MRAJEteqG",
+  database: "employees",
+  
+},
+    console.log('connected')
+);
+
+
+// con.connect(function(err) {
+//     if (err) throw err;
+//     console.log("Connected!");
+//   });
+
+
+
